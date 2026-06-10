d46d996982b64fe49174b62441ff949f98ea8302
Dawa tashi Sherpa <dawatasi149@gmail.com>
2026-06-10 18:46:41 +0545

    Complete a project

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Hello world");
+const joke = document.getElementById("meme");
+const button = document.getElementById("button");
+button.addEventListener("click", () => {
+  
+    const getUsers = async()=> {
+        let response = await fetch(" https://official-joke-api.appspot.com/random_joke");
+        let data = await response.json();
+ joke.innerHTML=data.setup + "<br>" + data.punchline;
+}
+
+getUsers();
+});
